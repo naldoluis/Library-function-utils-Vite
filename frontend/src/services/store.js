1f4930b934c3bb26439e6bc89ead14dc10a7aadc
naldoluis <javagithub@outlook.com>
2022-10-10 07:19:03 -0300

    funcoes e recursos para projetos futuro

--- frontend/src/services/store.js
+++ frontend/src/services/store.js
@@ -2,6 +2,6 @@ import thunk from 'redux-thunk'
 import rootReducer from './rootReducer'
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit'
 
-const store = configureStore(rootReducer, applyMiddleware(thunk))
+const store = configureStore({rootReducer, applyMiddleware},(thunk))
 
 export default store
