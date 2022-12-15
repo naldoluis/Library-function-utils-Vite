87f707310685ec8f0a7b586be04e8b4e4a396d1e
naldoluis <javagithub@outlook.com>
2022-12-15 18:13:10 -0300

    funcoes e recursos para projetos futuro

--- frontend/src/components/Bonus/Bonus.jsx
+++ frontend/src/components/Bonus/Bonus.jsx
@@ -11,30 +11,25 @@ export default function Bonus () {
 
     return (
       <>
-        <Card className="card-store" style={{ border: "2px solid #eee", borderRadius: "9px", width: "800px", margin: "auto" }}>
+        <Card style={{ border: "2px solid #eee", borderRadius: "7px", width: "800px", margin: "auto", background: "#383940" }}>
          <Card.Header>
           <Card.Body className="row" style={{ height: "205px" }}>
            <div>
-             <h5 style={{ fontFamily: "Varela Round", fontWeight: "bold", color: "#fff" }}>PROMOTIONAL CARD</h5>
-
-               <div style={{ border: ".1px solid #5e5e5e", height: "205px", width: "420px", background: "#383940", borderRadius: "5px", textAlign: "center" }}>
+             <h5 style={{ fontFamily: "Varela Round", fontWeight: "bold", color: "#fff" }}>⭐ ⭐PROMOTIONAL CARD⭐ ⭐</h5>
+               <div style={{ border: ".1px solid #5e5e5e", height: "200px", width: "420px", background: "#383940", borderRadius: "5px", textAlign: "center" }}>
                 <div style={{ fontFamily: "sans-serif", color: "#fff", fontSize: "12.7px" }}>
-
-                  <h4 className='text-warning' style={{ margin: "26px" }}>20% DISCOUNT</h4>
+                  <h4 style={{ margin: "26px", color: "#f5d20c" }}>20% DISCOUNT</h4>
                     <h6>Description : Kafka Streams in Action</h6>
                       <br></br>
                     <h6>William P. Bejeck Jr.</h6>
-                    <img src='https://www.danyprint.com.br/wp-content/uploads/2020/08/codigo-de-barras.png' style={{ width: "90px", height: "34px" }}/>
+                    <img src='https://www.danyprint.com.br/wp-content/uploads/2020/08/codigo-de-barras.png' style={{ width: "108px", height: "34px" }}/>
                   </div>
                  </div>
                 </div>
-              <img src='https://images.manning.com/360/480/resize/book/9/fd98cd2-f249-4675-989a-96736b84fef1/Bejeck-Kafka-HI.png' style={{ width: "200px", height: "224px", marginLeft: "70px", borderRadius: "7px" }}/>
+              <img src='https://images.manning.com/360/480/resize/book/9/fd98cd2-f249-4675-989a-96736b84fef1/Bejeck-Kafka-HI.png' style={{ width: "200px", height: "224px", marginLeft: "70px", borderRadius: "2px" }}/>
             </Card.Body>
-
             <Card.Footer style={{ textAlign: "right", color: "#fff" }}>
-             <div>
-               <h6 style={{ fontFamily: "Varela Round", color: "#fff", fontSize: "12px", textAlign: "right", paddingTop: "40px" }}>Serial Number: 0989-0798-7498-798-697-80.01</h6>
-             </div>
+               <h6 style={{ fontFamily: "Varela Round", color: "#fff", fontSize: "12px", paddingTop: "40px" }}>Serial Number: 0989-0798-7498-798-697-80.01</h6>
            </Card.Footer>
          </Card.Header>
         </Card>
