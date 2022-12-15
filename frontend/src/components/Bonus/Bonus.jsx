f069919f581bb0ad3b3cea1ff3a447b81a6f073b
naldoluis <javagithub@outlook.com>
2022-12-15 10:52:54 -0300

    funcoes e recursos para projetos futuro

--- frontend/src/components/Bonus/Bonus.jsx
+++ frontend/src/components/Bonus/Bonus.jsx
@@ -11,20 +11,20 @@ export default function Bonus () {
 
     return (
       <>
-        <Card className="card-store" style={{ border: "2px solid #eee", borderRadius: "20px" }}>
+        <Card className="card-store" style={{ border: "2px solid #eee", borderRadius: "10px", width: "800px", margin: "auto" }}>
          <Card.Header>
-          <Card.Body className="row" style={{ height: "385px" }}>
+          <Card.Body className="row" style={{ height: "205px" }}>
 
            <div>
-             <h4 style={{ fontFamily: "Varela Round", fontWeight: "bold", color: "#fff" }}>PROMOTIONAL CARD</h4>
+             <h5 style={{ fontFamily: "Varela Round", fontWeight: "bold", color: "#fff" }}>PROMOTIONAL CARD</h5>
            </div>
-              <img src='https://images.manning.com/360/480/resize/book/9/fd98cd2-f249-4675-989a-96736b84fef1/Bejeck-Kafka-HI.png' style={{ width: "250px", height: "344px", marginLeft: "500px", borderRadius: "7px" }}/>
+              <img src='https://images.manning.com/360/480/resize/book/9/fd98cd2-f249-4675-989a-96736b84fef1/Bejeck-Kafka-HI.png' style={{ width: "200px", height: "224px", marginLeft: "270px", borderRadius: "7px" }}/>
       
            </Card.Body>
 
             <Card.Footer style={{ textAlign: "right", color: "#fff" }}>
              <div>
-               <h6 style={{ fontFamily: "Varela Round", color: "#fff" }}>Serial Number: 0989-0798-7498-798-697-80.01</h6>
+               <h6 style={{ fontFamily: "Varela Round", color: "#fff", fontSize: "12px", textAlign: "right", paddingTop: "40px" }}>Serial Number: 0989-0798-7498-798-697-80.01</h6>
              </div>
            </Card.Footer>
          </Card.Header>
