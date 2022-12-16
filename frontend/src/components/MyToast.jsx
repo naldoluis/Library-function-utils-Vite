49ac47df0026458767a236b3d40b344be7417432
naldoluis <javagithub@outlook.com>
2022-12-16 16:52:09 -0300

    funcoes e recursos para projetos futuro

--- frontend/src/components/MyToast.jsx
+++ frontend/src/components/MyToast.jsx
@@ -5,8 +5,7 @@ export default function MyToast(props) {
   return (
     <div style={props.toast}>
       <Toast
-        className={`text-white ${
-          props.type === "success" ? "border-success bg-success" : "border-danger bg-danger"}`}
+        className={`text-white ${props.type === "success" ? "border-success bg-success" : "border-danger bg-danger"}`}
       >
         <Toast.Header className={`text-white ${props.type === "success" ? "bg-success" : "bg-danger"}`}
           closeButton={false}
