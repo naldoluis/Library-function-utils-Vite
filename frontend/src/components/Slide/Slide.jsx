ee1e55d541ef761624a9113b6bd608d9c1c56d25
naldoluis <javagithub@outlook.com>
2022-12-31 09:43:47 -0300

    funcoes e recursos para projetos futuro

--- frontend/src/components/Slide/Slide.jsx
+++ frontend/src/components/Slide/Slide.jsx
@@ -3,24 +3,24 @@ import { Carousel } from 'react-bootstrap'
 export default function Slide() {
   return (
     <Carousel>
-      <Carousel.Item>
+      <Carousel.Item className="slide">
        <h5 style={{ textAlign: "center" }}>News Page 1</h5>
         <img
-          style={{ height: 400, margin: "15px 0 0 290px", borderRadius: 5 }}
+          style={{ width: 700, height: 400, margin: "15px 0 0 200px", borderRadius: 5 }}
           src="https://miro.medium.com/max/1200/1*KzTdEF4HH8syparjt3vVwg.png"
         />
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item className="slide">
        <h5 style={{ textAlign: "center" }}>News Page 2</h5>
         <img
-          style={{ height: 400, margin: "15px 0 0 290px", borderRadius: 5 }}
+          style={{ width: 700, height: 400, margin: "15px 0 0 200px", borderRadius: 5 }}
           src="https://miro.medium.com/max/1200/1*_aGiGt0gYVUmE9QfUxjTgA.png"
         />
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item className="slide">
        <h5 style={{ textAlign: "center" }}>News Page 3</h5>
         <img
-          style={{ height: 400, margin: "15px 0 0 290px", borderRadius: 5 }}
+          style={{ width: 700, height: 400, margin: "15px 0 0 200px", borderRadius: 5 }}
           src="https://miro.medium.com/max/1200/1*WB8h3NUdG7qgSKJ74W9qyw.png"
         />
       </Carousel.Item>
