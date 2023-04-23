a7768b39d64188fd646c8126f5de19e596d69446
naldoluis <javagithub@outlook.com>
2023-04-23 12:10:41 -0300

    funcoes e recursos para projetos futuro

--- frontend/src/components/Weather/Weather.jsx
+++ frontend/src/components/Weather/Weather.jsx
@@ -155,7 +155,7 @@ export default function Weather() {
         <div className="input-group">
          <input className="form-control bg-dark text-white" placeholder={i18n.t('weather.input')} aria-label="Recipient's username" aria-describedby="button-addon2"/>
          <div className="card-footer">
-          <button style={{ marginTop: -18 }} className="btn btn-outline-success text-white bg-success" type="button">
+          <button style={{ marginTop: -18 }} className="btn btn-outline-success text-white bg-success" type="button" id="button-addon2">
             <FontAwesomeIcon icon={faSearch}/>
           </button>
         </div>
