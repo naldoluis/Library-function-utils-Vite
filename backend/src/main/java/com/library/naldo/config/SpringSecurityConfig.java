e4e05d73377cdb8f8c13b0ca46c17bdf9fd5491c
naldoluis <javagithub@outlook.com>
2023-05-28 06:20:46 -0300

    funcoes e recursos para projetos futuro

--- backend/src/main/java/com/library/naldo/config/SpringSecurityConfig.java
+++ backend/src/main/java/com/library/naldo/config/SpringSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -39,8 +38,8 @@ public class SpringSecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.cors();
-		http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().authorizeHttpRequests().requestMatchers("/user/authenticate").permitAll().anyRequest().authenticated();
+		http.csrf(csrf -> csrf.disable())
+		.authorizeHttpRequests().requestMatchers("/user/authenticate").permitAll().anyRequest().authenticated();
 		http.apply(new JwtTokenConfigurer(tokenProvider));
 		return http.build();
 	}
